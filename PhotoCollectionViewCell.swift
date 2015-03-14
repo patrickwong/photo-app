abb32bdc2ef9f6edf207aef8200d6990bfe79362
Patrick Wong <patrickwong@Patbookpro.local>
2015-03-14 13:56:51 -0700

    Add VCs
    Add VCs

---
+++ PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  photo app
+//
+//  Created by Patrick Wong on 3/14/15.
+//  Copyright (c) 2015 Patrick Wong. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ photo app/LibraryViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LibraryViewController.swift
+//  photo app
+//
+//  Created by Patrick Wong on 3/14/15.
+//  Copyright (c) 2015 Patrick Wong. All rights reserved.
+//
+
+import UIKit
+
+class LibraryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
